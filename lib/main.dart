514f0cc26c34ec4d7f79cf33d4ff56f17d062528
Prajjwal Veer Basnet <prajjwalveerbasnet10@gmail.com>
2023-10-27 23:15:16 +0545

    Connected Firebase Sucessfully with Flutter Project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tomato_game/firebase_options.dart';
 import 'package:tomato_game/login_page.dart';
 
 import 'home_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
